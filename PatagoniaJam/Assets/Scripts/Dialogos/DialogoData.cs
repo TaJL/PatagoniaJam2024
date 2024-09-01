2629d10f5debafc6001c7c5a262afb95d1cb23c3
Tadeo <tadeo@ravegan.com>
2024-08-31 22:20:56 -0300

    Agregado FMOD al proyecto

--- PatagoniaJam/Assets/Scripts/Dialogos/DialogoData.cs
+++ PatagoniaJam/Assets/Scripts/Dialogos/DialogoData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Patagonia/Dialogo")]
 public class DialogoData : ScriptableObject
 {
+    [SerializeField, Tooltip("Si esta casilla esta marcada, el dialogo se dispara solo cuando el conserge limpie la cantidad necesaria")]
+    private bool _dependeDeBasura = true;
     [SerializeField] private int _cantidadDeBasuraNecesaria;
     [SerializeField] private BurbujaDialogoData[] _burbujas = new BurbujaDialogoData[0];
 
